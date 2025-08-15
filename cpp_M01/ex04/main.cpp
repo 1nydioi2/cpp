fc26cc72ef2a41448c22baf6218d43a5c6d482fc
Nicolas Lambert-Euget <nilamber@c1r5p3.42nice.fr>
2025-08-15 17:30:31 +0200

    segfault miam

---
+++ cpp_M01/ex04/main.cpp
@@ -0,0 +1,36 @@
+#include "externals.hpp"
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		std::cout << "error: Number of argument not respected.\ncommand usage : \"./sifl <filename> <string to replace> <string replacing>\"." << std::endl;
+		return (1);
+	}
+	std::ifstream		file(argv[1]);
+	std::ostringstream	content_cp;
+	content_cp << file.rdbuf();
+	if (!file.good())
+	{
+		std::cout << "error: Could not open the file, make sure it exists.\n" << std::endl;
+		return (1);
+	}
+	std::string 		search = argv[2];
+	std::string 		str = content_cp.str();
+	std::string			replace = argv[3];
+	std::string			replaced_file = argv[1]; replaced_file += ".replace";
+	size_t				index = 0;
+	file.close();
+
+	while (index != std::string::npos)
+	{
+		index = str.find(search, index);
+		str.erase(index, search.length());
+		str.insert(index, replace);
+	}
+
+	std::ofstream file_replace(replaced_file.c_str());
+	file_replace << str;
+	file_replace.close();
+	return (0);
+}
