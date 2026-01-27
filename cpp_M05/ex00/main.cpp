09740429d81bed82983b9871274545934c8f7df3
cygwin <solokin2442@gmail.com>
2026-01-28 00:39:37 +0100

    added some tests, need to make sure objects are appropriately taken care of in case the start grade value is not conform

--- cpp_M05/ex00/main.cpp
+++ cpp_M05/ex00/main.cpp
@@ -5,23 +5,32 @@
 
 int main()
 {
-	Bureaucrat joseph( "Joseph", 150);
-	Bureaucrat laurene( "Laurene", 1);
+	Bureaucrat joseph( "Joseph", 150 );
+	Bureaucrat laurene( "Laurene", 1 );
+	Bureaucrat malo( "Malo", 0 );
+	std::cout << malo << std::endl;
+	Bureaucrat mael( "Mael", -1 );
+	std::cout << mael << std::endl;
+	Bureaucrat myl( "Myl", 151 );
+	std::cout << myl << std::endl;
 	Bureaucrat ano;
 
 	std::cout << std::endl;
 
 	std::cout << joseph << std::endl;
-	joseph.lowerGrade(1);
-	joseph.raiseGrade(2);
+	joseph.lowerGrade( 1 );
+	joseph.raiseGrade( 2 );
+	std::cout << joseph << std::endl;
 
 	std::cout << laurene << std::endl;
-	laurene.raiseGrade(1);
-	laurene.lowerGrade(2);
+	laurene.raiseGrade( 1 );
+	laurene.lowerGrade( 2 );
+	std::cout << laurene << std::endl;
 
 	std::cout << ano << std::endl;
-	ano.raiseGrade(1);
-	ano.lowerGrade(2);
+	ano.raiseGrade( 1 );
+	ano.lowerGrade( 2 );
+	std::cout << ano << std::endl;
 	
 	std::cout << std::endl;
 	return 0;
