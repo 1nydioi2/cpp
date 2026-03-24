47907865be3620a1724dcd178122f20812556c3f
cygwin <solokin2442@gmail.com>
2026-03-24 12:39:10 +0100

    started ex02

---
+++ cpp_M05/ex02/Bureaucrat.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Form.hpp"
+
+
+class	Form;
+
+class	Bureaucrat
+{
+	public:
+		Bureaucrat( void );
+		Bureaucrat( std::string name, int grade );
+		Bureaucrat( const Bureaucrat& source );
+		~Bureaucrat( void );
+
+		void			operator=( const Bureaucrat& other );
+
+		class	GradeTooLowException : public std::exception
+		{
+			public:
+				const char	*what( void ) const throw();
+		};
+
+		class	GradeTooHighException : public std::exception
+		{
+			public:
+				const char	*what( void ) const throw();
+		};
+		
+		std::string	getName( void ) const;
+		int			getGrade( void ) const;
+		void		raiseGrade( int x );
+		void		lowerGrade( int x );
+
+		void	signForm( Form &f );
+
+
+	private:
+		const std::string	_name;
+		int 				_grade;
+};
+
+std::ostream&	operator<<( std::ostream& out, const Bureaucrat& other );
