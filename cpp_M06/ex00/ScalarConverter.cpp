b42fd91db84655fdf73f853aa5d0ac5d0fa01088
ninjamint <solokin2442@gmail.com>
2026-04-22 09:38:27 +0200

    something to work on

--- cpp_M06/ex00/ScalarConverter.cpp
+++ cpp_M06/ex00/ScalarConverter.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <cstdlib>
 #include "ScalarConverter.hpp"
 
 
 
 void	ScalarConverter::convert( std::string literal )
 {
-	if ( std::stoi( literal ) > 32 && < 127 )
-		std::cout << "char: " << std::stoi( literal ) << "." << std::endl;
+	std::stringstream	lit;
+	int			liti;
+	float			litf;
+	double			litd;
+
+	lit << literal;
+	lit >> liti;
+	litf = std::atof( literal.c_str() );
+	litd = std::atof( literal.c_str() );
+	if ( liti > 32 && liti < 127 )
+		std::cout << "char: " << static_cast<char>(liti) << "." << std::endl;
 	else
 		std::cout << "char: Non displayable." << std::endl;
-	std::cout << literal "int: " << std::stoi( literal ) << "." << literal.stoi() << std::endl;
-	std::cout << literal "float: " << std::stoi( literal ) << "." << std::endl;
-	std::cout << literal "double: " << std::stoi( literal ) << "." << std::endl;
+	std::cout << "int: " << liti << "." << std::endl;
+	std::cout << "float: " << litf << "." << std::endl;
+	std::cout << "double: " << litd << "." << std::endl;
 
 	return ;
 }
