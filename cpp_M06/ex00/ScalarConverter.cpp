0e4abe83338041a64026fe5e398000d3e59666af
ninjamint <solokin2442@gmail.com>
2026-04-29 11:59:02 +0200

    cleaning output

--- cpp_M06/ex00/ScalarConverter.cpp
+++ cpp_M06/ex00/ScalarConverter.cpp
@@ -18,12 +18,12 @@ void	ScalarConverter::convert( std::string literal )
 	litf = std::atof( literal.c_str() );
 	litd = std::atof( literal.c_str() );
 	if ( liti > 32 && liti < 127 )
-		std::cout << "char: " << static_cast<char>(liti) << "." << std::endl;
+		std::cout << "char: " << static_cast<char>(liti) << std::endl;
 	else
 		std::cout << "char: Non displayable." << std::endl;
-	std::cout << "int: " << liti << "." << std::endl;
-	std::cout << "float: " << litf << "." << std::endl;
-	std::cout << "double: " << litd << "." << std::endl;
+	std::cout << "int: " << liti << std::endl;
+	std::cout << "float: " << litf << std::endl;
+	std::cout << "double: " << litd << std::endl;
 
 	return ;
 }
