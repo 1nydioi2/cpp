3f6fe34d7641114f4b92a33566e792603d248fde
Nicolas Lambert-Euget <nilamber@c1r7p2.42nice.fr>
2026-05-13 12:12:56 +0200

    ex00 finished

---
+++ cpp_M06/ex01/ScalarConverter.hpp
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+
+
+class	ScalarConverter
+{
+	public:
+		static void	convert( std::string literal );
+};
---
+++ cpp_M06/ex01/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include "ScalarConverter.hpp"
+
+
+
+int main( int argc, char *argv[])
+{
+	if ( argc != 2 )
+		return ( 1 );
+
+	std::string literal( argv[1] );
+	ScalarConverter::convert( literal );
+	ScalarConverter a;
+
+	(void) a;
+
+	return ( 0 );
+}
